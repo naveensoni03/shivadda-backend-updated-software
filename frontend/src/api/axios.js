2ebbb13feb3b8da68a959bddd768a075372a5c09
Naveen Soni <naveensoni03@gmail.com>
2026-02-15 22:17:07 +0530

    Updated axios baseURL to live render URL

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,15 +1,8 @@
 import axios from "axios";
-
 const api = axios.create({
-  // ✅ LOCALHOST (Abhi testing ke liye ye use karein)
-  baseURL: "http://127.0.0.1:8000/api/",
-  
-  // ❌ RENDER (Jab final live karna ho tab isse uncomment karein)
-  // baseURL: "https://naveen-education.onrender.com/api/",
-  
-  withCredentials: false, 
+    baseURL: "https://shivadda-backend-updated-software.onrender.com",
+    withCredentials: false
 });
-
 api.interceptors.request.use(
   (config) => {
     // ✅ Fix: Token name 'access_token' match kiya
